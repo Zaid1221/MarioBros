cead0667518630788efb1bed83acb2bf685e0b38
Zaid Javaid <zaid.javaid@gmail.com>
2017-11-27 20:12:25 -0500

    Initial commit

---
+++ core/src/com/zaid/mariobros/Scenes/Hud.java
@@ -0,0 +1,63 @@
+package com.zaid.mariobros.Scenes;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.zaid.mariobros.MarioBros;
+
+/**
+ * Created by Zaid on 11/27/2017.
+ */
+
+public class Hud {
+    public Stage stage;
+    private Viewport viewport;
+
+    private Integer worldTimer;
+    private float timeCount;
+    private Integer score;
+
+    Label countdownLabel;
+    Label scoreLabel;
+    Label timeLabel;
+    Label levelLabel;
+    Label worldLabel;
+    Label marioLabel;
+
+    public Hud(SpriteBatch sb){
+        worldTimer = 300;
+        timeCount = 0;
+        score = 0;
+
+        viewport = new FitViewport(MarioBros.V_WIDTH, MarioBros.V_HEIGHT, new OrthographicCamera());
+        stage = new Stage(viewport, sb);
+
+        Table table = new Table(); //creates the table where all the hud data will be shown
+        table.top(); //sets the table to be up top
+        table.setFillParent(true); // table is now size of stage
+
+        //sets the font of the lables up top
+        countdownLabel = new Label(String.format("%03d", worldTimer), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        scoreLabel = new Label(String.format("%06d", score), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        timeLabel = new Label("TIME", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        levelLabel = new Label("1-1", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        worldLabel = new Label("WORLD", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        marioLabel = new Label("MARIO", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+
+        table.add(marioLabel).expandX().padTop(10);
+        table.add(worldLabel).expandX().padTop(10);
+        table.add(timeLabel).expandX().padTop(10);
+        table.row();  //adds a new row to the table for the values of the labels
+        table.add(scoreLabel).expandX();
+        table.add(levelLabel).expandX();
+        table.add(countdownLabel).expandX();
+
+        stage.addActor(table); //this adds the table to the screen
+    }
+}
---
+++ core/src/com/zaid/mariobros/Screens/PlayScreen.java
@@ -0,0 +1,100 @@
+package com.zaid.mariobros.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.zaid.mariobros.MarioBros;
+import com.zaid.mariobros.Scenes.Hud;
+
+/**
+ * Created by Zaid on 11/27/2017.
+ */
+
+public class PlayScreen implements Screen {
+
+    private MarioBros game;
+    Texture texture;
+    private OrthographicCamera gamecam;
+    private Viewport gamePort;
+    private Hud hud;
+
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+
+    public PlayScreen(MarioBros game) {
+        this.game = game;
+        gamecam = new OrthographicCamera();
+        gamePort = new FitViewport(MarioBros.V_WIDTH, MarioBros.V_HEIGHT, gamecam);
+        hud = new Hud(game.batch);
+
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("level1.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        gamecam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    public void handleInput(float dt){
+        if(Gdx.input.isTouched())
+            gamecam.position.x += 100 * dt;
+    }
+
+    public void update(float dt){
+        handleInput(dt);
+
+        gamecam.update();
+        renderer.setView(gamecam);
+    }
+
+    @Override
+    public void render(float delta) {
+        update(delta);
+
+        Gdx.gl.glClearColor(1, 0, 0, 1); //color
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // clears the screen
+
+        renderer.render();
+
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        gamePort.update(width, height);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
